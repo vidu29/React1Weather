3708f39e3b513b46351d9899c49b0bf58a7f3d8f
vidu29 <mvidhya9122000@gmail.com>
2024-01-31 23:04:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import React, { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [deg, setdeg] = useState("degree")
+const [city, setcity] = useState("city")
+const [weather, setweather] = useState("weather")
+const [enteredvalue, sententeredvalue] = useState("")
+function handleInput(event) {
+  console.log(event.target.value)
+  sententeredvalue(event.target.value);
+}
+function getData() {
+  var weather = axios(`https://api.openweathermap.org/data/2.5/weather?q=${enteredvalue}&appid=a23ef0dd01f08cd935980037f55a3099`)
+  weather.then(function (dalta) {
+    console.log(dalta.data.main.temp);
+    setdeg(dalta.data.main.temp);
+    setcity(dalta.data.name);
+    setweather(dalta.data.weather[0].main);
+  })
+}
+return (
+  <div className="flex flex-row justify-center h-[100vh] items-center" >
+    <div style={{ backgroundImage: "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)" }} className="p-2 rounded-md shadow">
+      <h2 className="font-medium">Hey! ⛅</h2>
+      <p className="text-xs">Do you want to know the weather Report :)</p>
+      <input onChange={handleInput} type="text" className="rounded-md h-6 text-sm mt-2 p-1 outline-none" placeholder="City Name?" />
+      <br />
+      <button onClick={getData} className="bg-black text-white rounded-lg p-1 text-xs mt-2">Get Report ⚡</button>
+      <p className="text-xs mt-2">Degree: {deg} | City: {city} | Weather: {weather}</p>
     </div>
-  );
+  </div>)
 }
 
-export default App;
+export default App
